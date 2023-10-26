25039d4aae31be9435c5c09bf4a9186b042ef8a2
GabrielGesteiraa <149033137+GabrielGesteiraa@users.noreply.github.com>
2023-10-26 00:27:55 -0300

    FOLHADEPAGAMENTO.cob

--- FOLHADEPAGAMENTO.cob
+++ FOLHADEPAGAMENTO.cob
@@ -6,20 +6,20 @@
            DECIMAL-POINT IS COMMA.
        DATA DIVISION.
        WORKING-STORAGE SECTION.
-       01 NomeFuncionario PIC X(30).
-       01 HorasTrabalhadas PIC 9(3).
-       01 SalarioHora PIC 9(5)V99 VALUE 10.
-       01 SalarioBruto PIC 9(7)V99.
-       01 SALARIO-ED   PIC ZZZZZZ9,99.
-       01 ImpostoRenda PIC 9(5)V99.
-       01 IMPOSTO-ED   PIC ZZ999,99.
-       01 INSS PIC 9(5)V99.
-       01 INSS-ED PIC Z9999,99.
-       01 SalarioLiquido PIC 9(7)V99.
-       01 LIQUIDO-ED PIC ZZ99999,99.
+       01 NomeFuncionario    PIC X(30).
+       01 HorasTrabalhadas   PIC 9(3).
+       01 SalarioHora        PIC 9(5)V99 VALUE 10.
+       01 SalarioBruto       PIC 9(7)V99.
+       01 SALARIO-ED         PIC ZZZZZZ9,99.
+       01 ImpostoRenda       PIC 9(5)V99.
+       01 IMPOSTO-ED         PIC ZZ999,99.
+       01 INSS               PIC 9(5)V99.
+       01 INSS-ED            PIC Z9999,99.
+       01 SalarioLiquido     PIC 9(7)V99.
+       01 LIQUIDO-ED         PIC ZZ99999,99.
 
        PROCEDURE DIVISION.
-           DISPLAY "Sistema de Folha de Pagamento".
+            DISPLAY "Sistema de Folha de Pagamento".
             DISPLAY "Digite o nome do funcionario: ".
            ACCEPT NomeFuncionario.
             DISPLAY "Digite o numero de horas trabalhadas: ".
@@ -42,7 +42,7 @@
 
            DISPLAY "Folha de Pagamento para ", NomeFuncionario.
            DISPLAY "Salario Bruto: R$ ", SALARIO-ED.
-            DISPLAY "Imposto de Renda: R$ ", IMPOSTO-ED.
+           DISPLAY "Imposto de Renda: R$ ", IMPOSTO-ED.
            DISPLAY "INSS: R$ ", INSS-ED.
            DISPLAY "Salario Liquido: R$ ", LIQUIDO-ED.
 
